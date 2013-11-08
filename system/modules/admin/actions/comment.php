f263aee51e3428835b3a50081a23736f74869b0c
Adam Buckley <adam.buckley90@gmail.com>
2013-11-08 13:15:36 +1100

    Added standardised Comments section! (TaskComment code is still in for stability as i didnt have time to change it) It is used like: To get form: GET /admin/comment/[COMMENT_ID]/[TABLE_NAME]/[OBJECT_ID]?redirect_url=[REDIRECT_URL] where comment_id can be null

---
+++ system/modules/admin/actions/comment.php
@@ -0,0 +1,44 @@
+<?php
+
+function comment_GET(Web &$w){
+    $p = $w->pathMatch("comment_id", "tablename", "object_id");
+
+    $comment_id = intval($p["comment_id"]);
+    $comment = $comment_id > 0 ? $w->Comment->getComment($comment_id) : new Comment($w);
+    if ($comment === null){
+        $comment = new Comment($w);
+    }
+    
+    $form = array(
+        array("Comment","section"),
+        array("", "textarea", "comment", $comment->comment, 40, 15),
+        array("", "hidden", "redirect_url", urlencode($w->request("redirect_url")))
+    );
+
+    // return the comment for display and edit
+    $w->setLayout(null);
+    $w->out(Html::form($form, $w->localUrl("/admin/comment/{$p["comment_id"]}/{$p["tablename"]}/{$p["object_id"]}"), "POST", "Save"));
+}
+
+function comment_POST(Web &$w){
+    $p = $w->pathMatch("comment_id", "tablename","object_id");
+    $comment_id = intval($p["comment_id"]);
+    
+    $comment = $comment_id > 0 ? $w->Comment->getComment($comment_id) : new Comment($w);
+    if ($comment === null){
+        $comment = new Comment($w);
+    }
+    
+    $comment->obj_table = $p["tablename"];
+    $comment->obj_id = $p["object_id"];
+    $comment->comment = strip_tags($w->request("comment"));
+    $comment->insertOrUpdate();
+    
+    $redirectUrl = $w->request("redirect_url");
+
+    if (!empty($redirectUrl)){
+        $w->msg("Comment saved", urldecode($redirectUrl));
+    } else {
+        $w->msg("Comment saved", $w->localUrl($_SERVER["REQUEST_URI"]));
+    }
+}
---
+++ system/modules/admin/models/CommentService.php
@@ -0,0 +1,29 @@
+<?php
+
+class CommentService extends DbService {
+    
+    public function getCommentsForTable($table = null, $object_id = null){
+        $where = array("is_deleted" => 0);
+        if (!empty($table)){
+            if (is_a($table, "DbObject")){
+                // This way is probably better cause you dont hard code the table name in anywhere
+                $where["obj_table"] = $table->getDbTableName();
+            } else {
+                $where["obj_table"] = $table;
+            }
+            if (!empty($object_id)){
+                $where["obj_id"] = $object_id;
+            }
+            return $this->getObjects("Comment", $where);
+        }
+        return null;
+    }
+    
+    public function getComment($id = null){
+        if (!empty($id)){
+            return $this->getObject("Comment", array("id" => intval($id)));
+        }
+        return null;
+    }
+
+}
