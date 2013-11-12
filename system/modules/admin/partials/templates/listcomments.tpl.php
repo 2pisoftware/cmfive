bcc67e81c73dc4d3e8ebea92dde3a95f774300f8
Adam Buckley <adamb@joindup.com>
2013-11-12 12:14:03 +1100

    Made the layout of comments similar to basecamp, added deleting comments

--- system/modules/admin/partials/templates/listcomments.tpl.php
+++ system/modules/admin/partials/templates/listcomments.tpl.php
@@ -1,17 +1,24 @@
-            <?php
-                // Shows standard comment box, url is in the form:
-                // /admin/comment/[COMMENT_ID]/[TABLE_NAME]/[OBJECT_ID]?redirect_url=[REDIRECT_URL]
-                // Its a bit farfetched but provides us with a standard commenting interface
-                // Dont need to worry about urlencoding the redirect url
-                echo Html::box("/admin/comment//{$object->getDbTablename()}/{$object->id}?redirect_url=" . 
-                    $redirect, "Add Comment", true); ?>
-            <?php if (!empty($comments)) : ?>
-                <br/><br/>
-                <?php foreach($comments as $c) : ?>
-                    <?php echo $c; ?><br/><br/>
-                    <?php 
-                        // To edit comment, just add the ID
-                        echo Html::box("/admin/comment/{$c->id}/{$c->obj_table}/{$c->obj_id}?redirect_url=" . 
-                                $redirect, "Edit", true); ?><br/><br/>
-                <?php endforeach; ?>
-            <?php endif; ?>
+<?php
+    // Shows standard comment box, url is in the form:
+    // /admin/comment/[COMMENT_ID]/[TABLE_NAME]/[OBJECT_ID]?redirect_url=[REDIRECT_URL]
+    // Its a bit farfetched but provides us with a standard commenting interface
+    // Dont need to worry about urlencoding the redirect url
+    echo Html::box("/admin/comment//{$object->getDbTablename()}/{$object->id}?redirect_url=" . $redirect, "Add Comment", true);
+    if (!empty($comments)) : ?>
+        <div class="comment_container">
+            <?php foreach($comments as $c) : ?>
+                <div class="comment_section">
+                    <div class="comment_header"><b><?php echo @$c->w->Auth->getUser($c->creator_id)->getFullName(); ?></b></div>
+                    <div class="comment_body"><?php echo $c->comment; ?></div>
+                    <div class="comment_meta">
+                        Posted on <?php echo formatDate($c->dt_created, "d M \a\\t H:i"); ?>
+                        <?php if ($c->w->Auth->user()->id == $c->creator_id) {
+                            echo " - " . Html::box("/admin/comment/{$c->id}/{$c->obj_table}/{$c->obj_id}?redirect_url=" . $redirect, "Edit", false); 
+                            echo " or ";
+                            echo Html::a("/admin/deletecomment/{$c->id}?redirect_url=" . $redirect, "Delete", null, null, "Are you sure you want to delete this comment?"); 
+                        } ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
