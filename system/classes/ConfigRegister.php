f54b0e79cc4a6995b4d663dcd8f24bb072ca3465
Adam Buckley <adam.buckley90@gmail.com>
2014-04-07 10:42:33 +1000

    Finished DB Backup demo, need to make the commands config configurable (mysqldump) and introduce the option to backup to an external location

---
+++ system/classes/ConfigRegister.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * This class is responsible
+ *
+ *
+ */
--- system/modules/admin/actions/databasebackup.php
+++ system/modules/admin/actions/databasebackup.php
@@ -8,30 +8,36 @@ function databasebackup_ALL(Web $w) {
     
     AdminLib::navigation($w, "Database Backup");
     
-    $datestamp = date("Y-m-d-H");
+    $datestamp = date("Y-m-d-H-i");
     $filedir = SYSTEM_PATH . "/install/backups/";
     
     $dir = new DirectoryIterator($filedir);
     foreach ($dir as $fileinfo) {
         if (!$fileinfo->isDot()) {
             $filename = $fileinfo->getFilename();
-            $backuptime = DateTime::createFromFormat("Y-m-d-H\.\s\q\l", $filename);
-            if ($backuptime->getTimestamp() - time() < (60*60*4)) {
-                $w->out("You cannot backup more than once every 4 hours");
-                return;
+            try {
+                $backuptime = DateTime::createFromFormat("Y-m-d-H-i\.\s\q\l", $filename);
+                if ($backuptime) {
+                    if (time() - $backuptime->getTimestamp() < (60*60*4)) {
+                        $w->out("You cannot backup more than once every 4 hours");
+                        return;
+                    }
+                }
+            } catch (Exception $e) {
+                // Invalid timestamp
             }
         }
     }
     
     $filename = "$datestamp.sql";
     if (!strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-        echo system("mysqldump -u $MYSQL_USERNAME -p'$MYSQL_PASSWORD' $MYSQL_DB_NAME | gzip > {$filedir}{$filename}");
+        $w->out(shell_exec("mysqldump -u $MYSQL_USERNAME -p'$MYSQL_PASSWORD' $MYSQL_DB_NAME | gzip > {$filedir}{$filename}"));
     } else {
         // What to do with windows?
         // Either enter the path to mysqldump or create a shortcut thing
         // echo 'J:\xampp\mysql\bin\mysqldump.exe -u '.$MYSQL_USERNAME.' -p\''.$MYSQL_PASSWORD.'\' '.$MYSQL_DB_NAME.' > ' . "{$filedir}{$filename}";
         $w->out(exec('J:\xampp\mysql\bin\mysqldump.exe -u '.$MYSQL_USERNAME.' -p'.$MYSQL_PASSWORD.' ' . $MYSQL_DB_NAME . ' > ' . "{$filedir}{$filename}"));
-        $w->out("Backup completed to: {$filedir}{$filename}");
     }
+    $w->out("Backup completed to: {$filedir}{$filename}");
 //    $w->out($response);
 }
