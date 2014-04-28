9eb89fd3e4e3144d4561015de6f1e00aaf2ee408
Adam Buckley <adam.buckley90@gmail.com>
2014-04-28 10:11:34 +1000

    Moved backups to project root directory as they don't belong in system

--- system/modules/admin/actions/databasebackup.php
+++ system/modules/admin/actions/databasebackup.php
@@ -5,7 +5,7 @@ function databasebackup_ALL(Web $w) {
     $w->Admin->navigation($w, "Database Backup");
     
     $datestamp = date("Y-m-d-H-i");
-    $filedir = SYSTEM_PATH . "/install/backups/";
+    $filedir = ROOT_PATH . "/backups/";
     
     $dir = new DirectoryIterator($filedir);
     foreach ($dir as $fileinfo) {
