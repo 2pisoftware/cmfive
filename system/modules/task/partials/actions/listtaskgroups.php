68b2fb4d628682d32c3a549442788da6bf6a1826
Adam Buckley <adam.buckley90@gmail.com>
2015-01-30 13:50:44 +1100

    Made it easy to add a comment ($w->Comment->addComment($object, $message)), added improved filtering of task groups to minimise clutter of Task Dashboard/the partial that displays it

--- system/modules/task/partials/actions/listtaskgroups.php
+++ system/modules/task/partials/actions/listtaskgroups.php
@@ -1,6 +1,25 @@
 <?php 
 
 function listtaskgroups_ALL(Web $w, $params = array()) {
-    $w->ctx("taskgroups", $params['taskgroups']);
+    $taskgroups = $params['taskgroups'];
+    
+    $taskgroups = array_filter($taskgroups, function($taskgroup) use ($w) {
+        // First check if there are tasks
+        $tasks = $taskgroup->getTasks();
+        if (count($tasks) == 0) {
+            return false;
+        } else {
+            // Check if any of the tasks are accessible to the user
+            $tasks = array_filter($tasks, function($task) use ($w) {
+                return $task->getCanIView();
+            });
+            
+            // If there are tasks that the user can view then show the taskgroup
+            return (count($tasks) > 0);
+        }
+
+    });
+    
+    $w->ctx("taskgroups", $taskgroups);
     $w->ctx("redirect", $params['redirect']);
 }
