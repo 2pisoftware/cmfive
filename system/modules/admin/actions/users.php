a903b282789244bb284c6ce4d3b4a4c20149f12f
Adam Buckley <adam.buckley90@gmail.com>
2015-02-02 09:58:43 +1100

    Fixed bug where the contact id was being used instead of the user id to edit

--- system/modules/admin/actions/users.php
+++ system/modules/admin/actions/users.php
@@ -4,22 +4,21 @@ function users_GET(Web &$w) {
 
 	$header = array("Login","First Name","Last Name",array("Admin",true),array("Active",true),array("Created", true),array("Last Login", true),"Operations");
 //	$result = $w->db->sql("select user.id as id,login,firstname,lastname,is_admin,is_active,user.dt_created as dt_created,dt_lastlogin from user left join contact on user.contact_id = contact.id where user.is_deleted = 0 AND user.is_group = 0")->fetch_all();
-    $result = $w->db->get("user")->select("user.*, contact.*")->leftJoin("contact on user.contact_id = contact.id")->where("user.is_deleted", 0)->where("user.is_group", 0)->fetch_all();
-        $users = array();
-	foreach ($result as $user) {
-		$line = array();
-		$line[]=$user['login'];
-		$line[]=$user['firstname'];
-		$line[]=$user['lastname'];
-		$line[]= array($user['is_admin'] ? "X" : "", true);
-		$line[]=array($user['is_active'] ? "X" : "", true);
-		$line[]=array($user['dt_created'], true);
-		$line[]=array($user['dt_lastlogin'], true);
-		$view = Html::box($w->localUrl("/admin/useredit/".$user['id']."/box"),"Edit",true);
-		$view .= Html::b("/admin/permissionedit/".$user['id'],"Permissions");
-		$view .= Html::b($w->localUrl("/admin/userdel/".$user['id']),"Delete","Are you sure to delete this user?");
-		$line[]=$view;
-		$users[]=$line;
+        // $result = $w->db->get("user")->select("user.*, contact.*")->leftJoin("contact on user.contact_id = contact.id")->where("user.is_deleted", 0)->where("user.is_group", 0)->fetch_all();
+        $users = $w->Admin->getObjects("User", array("is_deleted" => 0, "is_group" => 0));
+        $data = array();
+	foreach ($users as $user) {
+            $contact = $user->getContact();
+            $data[] = array(
+                $user->login, $contact->firstname, $contact->lastname,
+                array($user->is_admin ? "X" : "", true),
+                array($user->is_active ? "X" : "", true),
+                array($w->Admin->time2Dt($user->dt_created), true),
+                array($w->Admin->time2Dt($user->dt_lastlogin), true),
+                Html::box($w->localUrl("/admin/useredit/".$user->id."/box"),"Edit",true) . 
+                Html::b("/admin/permissionedit/".$user->id,"Permissions") . 
+                Html::b($w->localUrl("/admin/userdel/".$user->id),"Delete","Are you sure to delete this user?")
+            );
 	}
-	$w->ctx("table",Html::table($users,null,"tablesorter",$header));
+	$w->ctx("table", Html::table($data, null, "tablesorter", $header));
 }
