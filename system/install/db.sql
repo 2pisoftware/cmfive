5a1101d987c353ea026914f6e4b3e29076875d09
careck <careck@gmail.com>
2015-08-21 15:44:07 +1000

    add sql for tags and favorites to install scripts

--- system/install/db.sql
+++ system/install/db.sql
@@ -770,3 +770,39 @@ CREATE TABLE IF NOT EXISTS `widget_config` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
+--
+-- Table structure for table `tag`
+--
+
+CREATE TABLE IF NOT EXISTS `tag` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `obj_class` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `obj_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `tag` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `tag_color` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `creator_id` bigint(20) DEFAULT NULL,
+  `dt_created` datetime DEFAULT NULL,
+  `modifier_id` bigint(20) DEFAULT NULL,
+  `dt_modified` datetime DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  INDEX deleted_tag_id(`is_deleted`, `tag`, `obj_class`, `obj_id`, `user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Table structure for table `favorite`
+--
+
+CREATE TABLE IF NOT EXISTS `favorite` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `object_class` varchar(255) NOT NULL,
+  `object_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `dt_created` datetime NOT NULL,
+  `creator_id` bigint(20) NOT NULL,
+  `dt_modified` datetime NOT NULL,
+  `modifier_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
