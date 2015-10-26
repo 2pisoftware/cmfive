620f40990a1b5fe8bfbdd06aa5c2e6622cdd67f4
syntithenai <syntithenai@gmail.com>
2015-10-27 09:13:32 +1100

    add tests for system and system/modules/admin

--- system/tests/_bootstrap.php
+++ system/tests/_bootstrap.php
@@ -1,9 +1,1 @@
 <?php
-$_SERVER['HTTPS'] = '';
-$_SERVER['HTTP_HOST'] = 'localhost';
-$_SERVER['DOCUMENT_ROOT'] = realpath(getenv('thisTestRun_testIncludePath'));
-
-
-chdir(getenv('thisTestRun_testIncludePath'));
-require_once "system\web.php";
-	
