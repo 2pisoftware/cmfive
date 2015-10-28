144021b06279f2d9df8eb4eed016d8e32aac35e6
syntithenai <syntithenai@gmail.com>
2015-10-28 15:49:14 +1100

    add acceptance tests for user create/edit/delete

--- system/tests/acceptance/adminCest.php
+++ system/tests/acceptance/adminCest.php
@@ -9,14 +9,17 @@ class adminCest
     public function _after()
     {
     }
+
     // auth details
 	var $username='admin';
 	var $password='admin';
 
-
-
-	public function login($I) {
+	public function testUserAdmin($I) {
 		$I->login($I,$this->username,$this->password);
+		$I->createUser($I,'testuser','password','testy','tererer','testy@tererer.com');
+		$I->setUserPermissions($I,'testuser',array('comment','user','favorites_user'));
+		$I->updateUser($I,'testuser',array('firstname'=>'Fred' ,'lastname'=>'Flintstone','check:is_admin'=>true));
+		$I->deleteUser($I,'testuser');
 	}
 
 }
