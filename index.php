4a34468b56f1c1dbfa15c2e7227a9a8eec2a71ea
syntithenai <syntithenai@gmail.com>
2015-11-24 11:38:53 +1100

    add c3 support for acceptance test coverage latest tests

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php 
 
 error_reporting(E_ALL);
+//putenv('thisTestRun_testRunnerPath=C:\inetpub\wwwroot\testrunner');
+// enable coverage for acceptance testing
+$c3File=getenv('thisTestRun_testRunnerPath').DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'c3.php';
+if (strlen(getenv('thisTestRun_testRunnerPath'))>0  && file_exists($c3File))  {
+	include($cFile);
+}
 
 require_once 'system/web.php';
 $web = new Web();
