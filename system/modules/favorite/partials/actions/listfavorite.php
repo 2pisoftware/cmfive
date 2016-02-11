5852de249528fb8c29f2932c40d3655ccbce8edc
Steve <steve@2pisoftware.com>
2016-02-12 07:56:40 +1100

    tweak favorites module to allow for searchpartial REST

--- system/modules/favorite/partials/actions/listfavorite.php
+++ system/modules/favorite/partials/actions/listfavorite.php
@@ -4,9 +4,30 @@
  * @author Steve Ryan steve@2pisoftware.com 2015
  */
 function listfavorite_ALL(Web $w,$params) {
-	// allow for REST data partial
-	if ($params['results']) {
-		$w->ctx('categorisedFavorites',$params['results']);
+	if (!empty($params['results']) && is_array($params['results']->success)) {
+		$user = $w->Auth->user();
+		if (!empty($user))  {
+			// convert rest results for template
+			$favoritesCategorised=array();
+			$service=new DBService($w);
+			
+			foreach ($params['results']->success as $v) {
+				$result=get_object_vars($v);
+				print_r(['RES',$result]);
+				$favorite= new Favorite($w);
+				$favorite->fill($result);
+				if (!array_key_exists($favorite->object_class,$favoritesCategorised)) $favoritesCategorised[$favorite->object_class]=array();
+				$realObject=$service->getObject($favorite->object_class,$favorite->object_id);
+				if (!empty($realObject))  {
+					$templateData=array();
+					$templateData['title']=$realObject->printSearchTitle();
+					$templateData['url']=$realObject->printSearchUrl();
+					$templateData['listing']=$realObject->printSearchListing();
+					if ($realObject->canList($user) && $realObject->canView($user)) array_push($favoritesCategorised[$favorite->object_class],$templateData);
+				}
+			}
+			$w->ctx('categorisedFavorites',$favoritesCategorised);
+		}
 	} else {
 		$user = $w->Auth->user();
 		if (!empty($user))  {
