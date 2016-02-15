d7df7fc135ce0848966eb3a54b808507eb9a6f73
Steve <steve@2pisoftware.com>
2016-02-16 01:49:04 +1100

    tweak doc

--- doc/snippets/register-for-timelog-favorites-tags.php
+++ doc/snippets/register-for-timelog-favorites-tags.php
@@ -3,6 +3,6 @@ $w->Timelog->registerTrackingObject($wiki);
 		
 // list tags		
 <?php echo $w->partial('listTags',['object' => $wiki], 'tag'); ?>
-// show the favorite button
+// show the cool favorite button
 <?php echo $w->Favorite->getFavoriteButton($wiki);?>
                 
