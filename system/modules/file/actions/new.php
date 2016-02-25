c4d7f3900d0c35847afc0ffaa5a4326513a8ad7e
careck <careck@gmail.com>
2016-02-26 08:38:40 +1100

    fix copy bug in file new action

--- system/modules/file/actions/new.php
+++ system/modules/file/actions/new.php
@@ -14,7 +14,7 @@ function new_GET(Web $w) {
 		'New Attachment' => [
 			[["File", "file", "file"]],
 			[["Title", "text", "title"]],
-			[["Description", "textarea", "description", $attachment->description,null,null,'justtext']]
+			[["Description", "textarea", "description", "",null,null,'justtext']]
 		]
 	];
 	
