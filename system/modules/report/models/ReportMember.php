d5be787bdd8076239f91940de8efd501603cdfea
Adam Buckley <adam.buckley90@gmail.com>
2016-02-25 17:06:49 +1100

    Updated missing report_member object from merge conflict

--- system/modules/report/models/ReportMember.php
+++ system/modules/report/models/ReportMember.php
@@ -1,18 +1,17 @@
 <?php
-// report member object
+
 class ReportMember extends DbObject {
+	
 	public $report_id;		// report id
 	public $user_id; 		// user id
 	public $role;			// user role: user, editor
 	public $is_email_recipient;
 	public $is_deleted; 	// deleted flag
 
-	public static $_db_table = "report_member";
-	// actual table name
-	public function getDbTableName() {
-		return "report_member";
+	public function getReport() {
+		return $this->getObject("Report", $this->report_id);
 	}
-	
+
 	public function getUser() {
 		return $this->getObject("User", ['id' => $this->user_id, "is_deleted" => 0]);
 	}
