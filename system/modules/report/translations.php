82c66f39a401de601406669bbd8ca1d58d5403ed
Steve <steve@2pisoftware.com>
2016-03-05 23:28:56 +1100

    translations proposal, NOTE /report/connections/index action has been hacked/broken to show translation function cases

---
+++ system/modules/report/translations.php
@@ -0,0 +1,16 @@
+<?php
+
+Config::set('translations',
+	array('report' => array(
+		'permodule' => 'per module',
+		'index' => array(
+			'peraction' => 'per action'
+		),
+		'connections' => array(
+			'persub' => 'per sub module',
+			'index' => array(
+				'persubaction' => 'per sub action'
+			)
+		)
+		
+)));
