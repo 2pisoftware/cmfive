fc3e69e3ec283ae36ada9808895ccf1b518addba
isaac <iceaxe.liehne@gmail.com>
2016-10-05 16:16:27 +1100

    initial work for comment notifications

--- system/modules/admin/actions/comment.php
+++ system/modules/admin/actions/comment.php
@@ -31,14 +31,35 @@ Horizontal Line:
 ---
 EOF;
     
+    //call hook for notification select
+    $results = $w->callHook('comment', 'get_notification_recipients_' . $p['tablename'],['object_id'=>$p['object_id']]);
+    
     $form = array(
         array("Comment","section"),
         array("", "textarea", "comment", $comment->comment, 100, 15, false),
     	array("Help","section"),
     	array("", "textarea", "-help",$help, 100, 5, false),
         array("", "hidden", "redirect_url", $w->request("redirect_url"))
+        
     );
+    //add checkboxes to the form for each notification recipient
+    var_dump($results);
+    if (!empty($results)) {
+        foreach($results as $recipients) {
+            $form[] = array("Notifications","section");
+            $form[] = array("", "hidden", "is_notifications", 1);
+            foreach ($recipients as $user_id => $is_notify) {
+                $user = $w->Auth->getUser($user_id);
+                if (!empty($user)) {
+                    $form[] = array($user->getFullName() . '    ', 'checkbox', 'recipient_' . $user->id, $is_notify);
 
+
+                }
+            }
+        }
+    }
+    
+    
     // return the comment for display and edit
     $w->setLayout(null);
     $w->out(Html::form($form, $w->localUrl("/admin/comment/{$comment_id}/{$p["tablename"]}/{$p["object_id"]}"), "POST", "Save"));
@@ -57,6 +78,20 @@ function comment_POST(Web $w){
     $comment->obj_id = $p["object_id"];
     $comment->comment = strip_tags($w->request("comment"));
     $comment->insertOrUpdate();
+    
+    //handle notifications
+    if($w->request("is_notifications")) {        
+        $recipients = [];        
+        foreach($_POST as $key=>$value) {
+            $exp_key = explode('_',$key);
+            if ($exp_key[0] == 'recipient') {
+                $recipients[] = $exp_key[1];
+            }            
+        }        
+        $results = $w->callHook('comment', 'send_notification_recipients_' . $p['tablename'],['object_id'=>$p['object_id'], 'recipients'=>$recipients, 'commentor_id'=>$w->auth->loggedIn(),'comment'=>$comment]);
+    
+        
+    }
     
     $redirectUrl = $w->request("redirect_url");
 
