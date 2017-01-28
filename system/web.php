5c52893072ea6713e19d2215f1226644d2ff95cc
Carsten Eckelmann <careck@gmail.com>
2017-01-28 14:01:10 +1100

    updated Web::checkUrl() to accept array of actions

--- system/web.php
+++ system/web.php
@@ -1905,6 +1905,10 @@ class Web {
 	 * 
 	 * Passing "*" to module, submodule or action allows any.
 	 * 
+	 * To skip a submodule, pass in "" as parameter
+	 * 
+	 * $action can be an array of actions
+	 * 
 	 * @param string $url
 	 * @param string $module 
 	 * @param string $submodule
@@ -1916,13 +1920,31 @@ class Web {
     	if (empty($p) || empty($module)) {
     		return false;
     	}
-    	if ($action != $p['action'] && $action != "*") {
-    		return false;
-    	}
-       	if ($submodule != $p['submodule'] && $submodule != "*") {
-    		return false;
-    	}
+    	// first check the module
         if ($module != $p['module'] && $module != "*") {
+    		return false;
+    	}
+    	
+    	// then check the submodule
+    	if ($submodule != $p['submodule'] && $submodule != "*") {
+    		return false;
+    	}
+    	 
+    	// now check actions
+    	if (empty($action)) {
+    		// if no action has been passed in for checking then assume anything is allowed
+    		return true;
+    	}
+   		if (is_array($action)) {
+   			// loop through all actions until we find one that matches
+   			$action_gate = false;
+   			foreach ($action as $ac) {
+   				if ($ac == $p['action']) {
+   					$action_gate = true;
+   				}
+   			}
+   			return $action_gate;
+   		} else if ($action != $p['action'] && $action != "*") {
     		return false;
     	}
     	return true;
